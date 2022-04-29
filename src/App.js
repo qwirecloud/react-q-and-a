56458eda746ac1972b9e95bb04d5a35d59f258fb
Bruno Krebs <krebs.bruno@gmail.com>
2022-04-29 16:18:12 -0300

    feat: first task manager version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useAtom } from 'jotai'
+
+import { tasksAtom } from './atoms';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useAtom(tasksAtom)
   return (
-    <div>Getting Started</div>
+    <div>
+      <h1>Tachometer</h1>
+      <p>Measure your task-completion velocity.</p>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.key}>{task.title}</li>
+        ))}
+      </ul>
+      <div>
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          const { taskTitleField } = event.target;
+          console.log(taskTitleField.value)
+          if (!taskTitleField.value) {
+            return;
+          }
+          setTasks([...tasks, { key: Date.now(), title: taskTitleField.value }])
+          taskTitleField.value = '';
+        }}>
+          <label>New Task:</label>
+          <input name="taskTitleField" type="text" maxLength={30} />
+          <button name="createTaskButton" type='submit'>Create</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
