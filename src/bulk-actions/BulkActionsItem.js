3367a5c744aa4966fcade54dee5dd86d73694096
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2023-02-17 21:42:39 +0000

    Implement select one and select all items

--- src/bulk-actions/BulkActionsItem.js
+++ src/bulk-actions/BulkActionsItem.js
@@ -1,17 +1,29 @@
 const options = { year: "numeric", month: "long", day: "numeric" };
 
-export function BulkActionsItem({ item }) {
-  const { title, description, dueDate } = item;
+export function BulkActionsItem({ item, toggleSelect }) {
+  const { title, description, dueDate, id, selected } = item;
 
   return (
     <li className="px-2 py-1 hover:bg-gray-100">
-      <div>{title}</div>
-      <div className="flex justify-between">
-        <div className="text-gray-600 text-sm">
-          <p>{description}</p>
-        </div>
-        <div className="text-sm">
-          {new Intl.DateTimeFormat("en-US", options).format(new Date(dueDate))}
+      <div className="flex space-x-2">
+        <input
+          type="checkbox"
+          name="selected"
+          checked={selected}
+          onChange={() => toggleSelect(id)}
+        />
+        <div>
+          <div>{title}</div>
+          <div className="flex justify-between">
+            <div className="text-gray-600 text-sm">
+              <p>{description}</p>
+            </div>
+            <div className="text-sm">
+              {new Intl.DateTimeFormat("en-US", options).format(
+                new Date(dueDate)
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </li>
